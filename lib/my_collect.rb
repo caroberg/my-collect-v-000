75d5b79a63283e0866355453302135fb8b002235
Caroline Berg <carolineeberg@gmail.com>
2018-08-08 02:28:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,7 @@
 collection = ["John Smith", "Yoshi Bara", "Zed Nugget"]
 
+collection = ["John Smith", "Yoshi Bara", "Zed Nugget"]
+
 def my_collect(collection)
   i = 0
   new_array = []
@@ -7,11 +9,13 @@ def my_collect(collection)
   while i < collection.length
     yield collection[i]
     i += 1
-  new_array << collection[i - 1]
+  new_array << collection[i - 1].upcase
+  second_array << collection[i - 1].split(" ").first
   end
   new_array
+  second_array
 end
 
 my_collect(collection) do |i|
-  array["#{i}"]
+  i
 end
