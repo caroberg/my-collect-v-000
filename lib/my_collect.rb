bbacac3202b9792c8a14d2702423771de159f670
Caroline Berg <carolineeberg@gmail.com>
2018-08-08 02:09:34 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,15 +3,14 @@ collection = ["John Smith", "Yoshi Bara", "Zed Nugget"]
 def my_collect(collection)
   i = 0
   new_array = []
-  second_array = []
+
   while i < collection.length
     yield collection[i]
     i += 1
-  new_array << collection[i - 1].upcase
-  second_array << collection[i - 1].split(" ").first
+  new_array << collection[i - 1]
   end
   new_array
-  second_array
+
 end
 
 my_collect(collection) do |i|
